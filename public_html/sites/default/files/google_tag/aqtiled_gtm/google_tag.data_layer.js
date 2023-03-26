42e5d6547270536a27b6c3fc7b2dad673d26bc8b
Stefan Carlsson <stefan@adrete.se>
2023-03-26 09:28:20 +0200

    9.5.7 again

--- public_html/sites/default/files/google_tag/aqtiled_gtm/google_tag.data_layer.js
+++
@@ -1,1 +0,0 @@
-(function(w,l){w[l]=w[l]||[];w[l].push({"gtm.whitelist":["google","nonGooglePixels","nonGoogleScripts","nonGoogleIframes"],"gtm.blacklist":["customScripts","customPixels"]});})(window,'dataLayer');
